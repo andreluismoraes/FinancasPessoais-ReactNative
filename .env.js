63358aa3b1d499d598ea5dbb839fcba0e81c5f0e
andreluismoraes <andreluismoraes500@gmail.com>
2020-08-24 22:59:53 -0300

    configurando .env no react native e configurando o services para aceitar os arquivos do .env

---
+++ .env.js
@@ -0,0 +1,9 @@
+API_KEY = "AIzaSyAgaip0XYB8wtWIEU0twnBC5UkKwE9v4Pc"
+AUTH_DOMAIN = "inz-informatica.firebaseapp.com"
+DATABASE_URL = "https://inz-informatica.firebaseio.com"
+PROJECT_ID = "inz-informatica"
+STORAGE_BUCKET = "inz-informatica.appspot.com"
+MESSAGING_SENDER_ID = "1002420267148"
+APP_ID = "1:1002420267148:web:6a69dd19858949a90eb3a9"
+MEASUREMENT_ID = "G-D2YRGKZ2QQ"
+    
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,17 +1,25 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-require("dotenv").config();
+
+const API_KEY = process.env["API_KEY"];
+const AUTH_DOMAIN = process.env["AUTH_DOMAIN"];
+const DATABASE_URL = process.env["DATABASE_URL"];
+const PROJECT_ID = process.env["PROJECT_ID"];
+const STORAGE_BUCKET = process.env["STORAGE_BUCKET"];
+const MESSAGING_SENDER_ID = process.env["MESSAGING_SENDER_ID"];
+const APP_ID = process.env["APP_ID"];
+const MEASUREMENT_ID = process.env["MEASUREMENT_ID"];
 
 let firebaseConfig = {
-  apiKey: process.env.apikey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
