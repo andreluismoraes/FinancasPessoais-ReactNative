75dac6e60782807dfe46b981ebe313f712e5137c
andreluismoraes <andreluismoraes500@gmail.com>
2020-08-24 21:51:07 -0300

    Configurando arquivo .env e modificando o arquivo services para o configuração do .env

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,6 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+require("dotenv").config();
+
+let firebaseConfig = {
+  apiKey: process.env.apikey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
